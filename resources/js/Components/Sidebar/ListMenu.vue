2a270b3b2a9b863c5b53aba812178af50646a9b0
bagaswihant <bagaswihant@gmail.com>
2024-05-17 22:56:47 +0700

    fix: template & navbar

--- resources/js/Components/Sidebar/ListMenu.vue
+++ resources/js/Components/Sidebar/ListMenu.vue
@@ -1,99 +1,75 @@
 <script setup>
 import { ref } from 'vue';
-import AppMenuItem from '@/primevue/AppMenuItem.vue';
 import { Link } from '@inertiajs/vue3';
-import NavLink from '../NavLink.vue';
+import PanelMenu from 'primevue/panelmenu';
 
-const submenuShow = ref(true)
+const activeUrl = (alamat, url1,item) => {
+    return alamat.includes(url1) ? 'bg-blue-300 dark:bg-blue-800' : '';
+}
 
-const model = ref([
+const items = ref([
     {
-        label: 'Master',
-        drop:false,
-        child: [
-            {
-                label: 'Master Barang',
-                to: route('barang.index'),
-                icon: 'pi pi-fw pi-chart-bar',
-                child: null
-            },
-            {
-                label: 'Master Puer',
-                to: 'barang',
-                icon: 'pi pi-fw pi-chart-bar',
-                child: null
-            },
-        ],
-        to: null,
-        icon: 'pi pi-fw pi-chart-bar'
-    },
-    {
-        label: 'Dashboard',
-        child: null,
-        to: route('/'),
-        icon: 'pi pi-fw pi-chart-bar'
+        label: 'Dasboard',
+        icon: 'pi pi-home',
+        url: route('/')
     },
     {
         label: 'Master',
-        drop:false,
-        child: [
+        icon: 'pi pi-palette',
+        active:false,
+        items: [
             {
-                label: 'Master Barang',
-                to: route('barang.index'),
-                icon: 'pi pi-fw pi-chart-bar',
-                child: null
+                label: 'Barang',
+                icon: 'pi pi-eraser',
+                url: route('barang.index')
             },
             {
-                label: 'Master Puer',
-                to: 'barang',
-                icon: 'pi pi-fw pi-chart-bar',
-                child: null
+                label: 'Perusahaan',
+                icon: 'pi pi-heart',
+                url: route('dashboard')
+            }
+        ]
+    },
+    {
+        label: 'Laporan',
+        icon: 'pi pi-palette',
+        active:false,
+        items: [
+            {
+                label: 'Rab',
+                icon: 'pi pi-eraser',
+                url: route('barang.index')
             },
-        ],
-        to: null,
-        icon: 'pi pi-fw pi-chart-bar'
+            {
+                label: 'Purchase Order',
+                icon: 'pi pi-heart',
+                url: route('barang.index')
+            },
+            {
+                label: 'Invois',
+                icon: 'pi pi-heart',
+                url: route('barang.index')
+            }
+        ]
     },
 ]);
-
-const activeUrl = (url,url1)=>{
-    // console.log(url,url1);
-    return url.includes(url1) ? 'bg-blue-300' : '';
-}
-
-const dropShowOff = (status)=>{
-    
-    status.drop = status.drop ? false : true
-}
 </script>
 
 
 <template>
     <ul class="list-none p-0 m-0 overflow-hidden">
-        <li v-for="menu in model">
-            <a v-if="menu.child"  @click="dropShowOff(menu)"
-                class="flex items-center cursor-pointer p-3 rounded-md text-surface-700 dark:text-surface-0/80 hover:bg-surface-100 dark:hover:bg-surface-700 duration-200 transition-colors">
-                <i class="pi pi-chart-line mr-2"></i>
-                <span class="font-medium">{{ menu.label }}</span>
-                <i class="pi pi-chevron-down ml-auto"></i>
-            </a>
-            <Link :href="menu.to" v-ripple v-else
-                class="flex items-center cursor-pointer p-3 rounded-md text-surface-700 dark:text-surface-0/80 hover:bg-surface-100 dark:hover:bg-surface-700 duration-200 transition-colors "
-                :class="activeUrl(menu.to,$page.url)">
-                <i class="pi pi-cog mr-2"></i>
-                <span class="font-medium">{{ menu.label }}</span>
-            </Link>
-            {{ menu.drop }}aa
-            <ul class="list-none py-0 pl-3 pr-0 m-0 overflow-y-hidden transition-all duration-400 ease-in-out" v-if="menu.drop" :class="[menu.drop ? 'h-auto':'']">
-                <li v-for="md in menu.child">
-                    <Link v-ripple :href="md.to"
-                    :class="activeUrl(md.to,$page.url)"
-                        class="flex items-center cursor-pointer p-3 rounded-md text-surface-700 dark:text-surface-0/80 hover:bg-surface-100 dark:hover:bg-surface-700 duration-200 transition-colors">
-                        <i class="pi pi-chart-line mr-2"></i>
-                        <span class="font-medium">{{ md.label }}</span>
+        <PanelMenu :model="items"  >
+            <template #item="{ item }" >
+                <Link v-if="item.url" :href="item.url" v-ripple :class="activeUrl(item.url, $page.url)"
+                class="flex items-center cursor-pointer p-3 text-surface-700 dark:text-surface-0/80 hover:bg-surface-100 dark:hover:bg-surface-700 duration-200 transition-colors ">
+                {{ item.label }}
                 </Link>
-                </li>
-            </ul>
-
-        </li>
+                <a v-else v-ripple class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0/80 px-3 py-2" :class=" item.items.map((i) => activeUrl(i.url, $page.url)) ">
+                    <span :class="item.icon" />
+                    <span class="ml-2">{{ item.label }}</span>
+                    <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
+                </a>
+            </template>
+        </PanelMenu>
     </ul>
 </template>
--- resources/js/Layouts/Base.vue
+++ resources/js/Layouts/Base.vue
@@ -1,9 +1,17 @@
 <template>
-<SidebarMenu>
+    
+    <div class="flex flex-nowrap">
+        <SidebarXL />
 
-</SidebarMenu>
+        <!-- SIDEBAR PHONE -->
+        <div class="container mx-auto p-2 h-64 md:w-4/5 md:px-4">
+            <SidebarResponsive />
+            <slot />
+        </div>
+    </div>
 </template>
 
 <script setup>
-import SidebarMenu from '@/Components/Sidebar/SidebarMenu.vue';
+import SidebarResponsive from '@/Components/Sidebar/SidebarResponsive.vue';
+import SidebarXL from '@/Components/Sidebar/SidebarXL.vue';
 </script>
